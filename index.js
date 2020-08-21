e38abe71c5fcab37dc08a4315c89f679ea279ad1
Parker Williamson <parker.williamson@gmail.com>
2020-08-21 18:04:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+const createEmployeeRecord = info => {
+  const record = {
+    firstName: info[0],
+    familyName: info[1],
+    title: info[2]
+    payPerHour: info[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return record;
+};
+
+const createEmployeeRecords = array => array.map( employee => createEmployeeRecord(employee))
