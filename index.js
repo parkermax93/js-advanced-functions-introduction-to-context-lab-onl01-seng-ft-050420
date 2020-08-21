321f2630545c65ae0e6ccfa31681908330e07440
Parker Williamson <parker.williamson@gmail.com>
2020-08-21 18:12:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,35 @@ const createEmployeeRecord = info => {
 
 const createEmployeeRecords = array => array.map( employee => createEmployeeRecord(employee))
 
-const createTimeInEvent
+const createTimeInEvent = (employee, dateStamp) => {
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    date: dateStamp.split(' ')[0]
+    hour: parseInt(dateStamp.split(' ')[1]),
+  });
+  return employee
+};
+
+const createTimeOutEvent = (employee, dateStamp) => {
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    date: dateStamp.split(' ')[0]
+    hour: parseInt(dateStamp.split(' ')[1]),
+  });
+  return employee 
+};
+
+const hoursWorkedOnDate = (employee, date) => {
+  let timeIn = employee.timeInEvents.find( e => e.date === date)
+  let timeOut = employee.timeOutevents.find( e => e.date === date)
+  let hours = parseInt(timeOut.hour = timeIn.hour)/100
+  return hours 
+}
+
+const wagesEarnedOnDate = (employee, date) => {
+  return parseInt(hoursWorkedOnDate(employee, date) * employee.payPerHour);
+};
+
+const allWagesFor = employee => {
+  const dates = employee.timeInEvents.map(e => e.date);
+};
